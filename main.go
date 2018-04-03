d2d9d418eab5cf5792b03283cecf9bc51223f984
Chris Tarazi <tarazichris@gmail.com>
2018-04-02 21:34:29 -0700

    Create and use log_entry struct

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"regexp"
-	"strings"
+	"time"
 )
+
+type log_entry struct {
+	ip        string
+	date      string
+	time      string
+	timestamp time.Time
+	action    string
+}
+
+func create_entry(ip, date, timestr, action []byte) log_entry {
+	dt := string(date) + " " + string(timestr)
+	ts, _ := time.Parse("02/Jan/2006 15:04:05", dt)
+	return log_entry{string(ip),
+		string(date),
+		string(timestr),
+		ts,
+		string(action)}
+}
 
 func read_file(filename string) []byte {
 	if _, err := os.Stat(filename); os.IsNotExist(err) {
@@ -23,18 +42,23 @@ func read_file(filename string) []byte {
 	return data
 }
 
-func parse(content []string) ([]string, []string) {
+func parse(content [][]byte) ([]log_entry, [][]byte) {
 	restr := "(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}) - - " +
 		"\\[(\\d{1,2}\\/\\w{3}\\/\\d{4}):(\\d{2}:\\d{2}:\\d{2}).+" +
 		"(\\\"GET.+\\\"){1,3}"
 
-	matches := []string{}
-	nonmatches := []string{}
+	matches := []log_entry{}
+	nonmatches := [][]byte{}
 
 	regex, _ := regexp.Compile(restr)
 	for _, v := range content {
-		if len(regex.FindString(v)) != 0 {
-			matches = append(matches, v)
+		if len(regex.Find(v)) != 0 {
+			submatches := regex.FindAllSubmatch(v, -1)[0]
+			ip := submatches[1]
+			date := submatches[2]
+			time := submatches[3]
+			action := submatches[4]
+			matches = append(matches, create_entry(ip, date, time, action))
 		} else {
 			nonmatches = append(nonmatches, v)
 		}
@@ -49,8 +73,8 @@ func main() {
 		os.Exit(1)
 	}
 
-	data := string(read_file(os.Args[1]))
-	lines := strings.Split(data, "\n")
+	data := read_file(os.Args[1])
+	lines := bytes.Split(data, []byte("\n"))
 
 	m, n := parse(lines)
 
